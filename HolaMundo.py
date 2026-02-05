fe5fa9f347e7ed5c0fa0f5567e26ddbf2a1e3219
Néstor Velarde Gonzales <u20211c221@upc.edu.pe>
2026-02-05 15:54:40 -0500

    feat: We added list exercises in Python, functionalities.

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,1 +1,49 @@
-print("Hola Mundo"); 
+print("Hola Mundo");
+print("---------------------------");
+
+message = "Hola al mundo de developers";
+print(message);
+print("---------------------------");
+
+name = "nestor velarde - codydev";
+print(name.title());  # Cambia a cada palabra con inicial Mayúscula
+print(name.upper());  # Pasa a todo MAYUSCULA la cadena
+print(name.lower());  # Pasa a todo Minuscula la cedena
+print("---------------------------");
+
+first_name = "nestor";   # USO DE VARIABLES DE CADENA con cadenas F
+last_name = "velarde gonza";
+full_name = f"{first_name} {last_name}";
+print(f"Hello!, {full_name.title()}!");
+
+print("---------------------------");
+
+numero_largo = 123_235_987; # Guiones bajos en numeros largos
+print(numero_largo);
+
+print("---------------------------");
+a, b, c = 0, 0, 0; # Asignación multiple
+
+print("---------------------------");
+MAX_CONNECTIONS = 100; # Variable constante y nunca alterarse
+
+import this # Informaciones
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Listas.py
@@ -0,0 +1,30 @@
+# LISTAS, una colección de elementos dispuestos en un orden particular
+# El uso de [] indica una lista separada por comas (,)
+
+tech = ['AWS', 'azure', 'Google Cloud', 'Oracle'];
+print(tech[3]);
+print(tech[1].title().upper());
+print(tech[-1]); # Accede al ultimo lista
+message = f"Mi favorite Cloud is: {tech[1].upper()}";
+print(message);
+
+print("------------ EJERCICIO ---------------");
+amigos = ['Brayan', 'Lizbeth', 'Maria', 'Julia'];
+vehiculos = ['moto', 'bicicleta', 'carro', 'avión'];
+print(amigos);
+messages = f"A {amigos[1].title()} le gusta {vehiculos[0]} para viajar";
+print(messages);
+
+# MODIFICAR, AÑADIR Y ELIMINAR ELEMENTOS
+
+
+
+
+
+
+
+
+
+
+
+
